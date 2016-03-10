d53914d15f50e36e18e1fd2dda914f2b21a7b93f
Paul Iacomi <iacomi.paul@gmail.com>
2016-03-10 16:37:32 +0100

    Deleted everything

--- Kaolla/Define_Manip_Vannes.h
+++
@@ -1,7 +0,0 @@
-#ifndef DEFINE_MANIP_VANNES_H
-#define DEFINE_MANIP_VANNES_H
-
-#define temps_ouverture 1000//ms = 1s
-#define temps_fermeture temps_ouverture // => temps_fermeture identique a temps ouverture
-
-#endif
