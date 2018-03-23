661930d0953d0e3a399b906072978d889a3de2d5
Paul Iacomi <paul.iacomi@outlook.com>
2018-03-23 17:07:49 +0100

    connection port

--- Kalel/resourceConnectionPort.h
+++ Kalel/resourceConnectionPort.h
@@ -2,25 +2,30 @@
 // Microsoft Visual C++ generated include file.
 // Used by ConnectionPort.rc
 //
-
-// Edit boxes
+#define IDC_CHECK_T1                    8001
+#define IDC_CHECK_T2                    8002
+#define IDC_CHECK_T3                    8003
+#define IDC_CHECK_P1                    8004
+#define IDC_CHECK_P2                    8005
+#define IDC_CHECK_C1                    8006
 #define IDC_EDIT_T1                     8010
 #define IDC_EDIT_T2                     8011
 #define IDC_EDIT_T3                     8012
 #define IDC_EDIT_P1                     8013
 #define IDC_EDIT_P2                     8014
 #define IDC_EDIT_C1                     8015
-
-// Checkboxes
-#define IDC_CHECK_T1                    8024
-#define IDC_CHECK_T2                    8025
-#define IDC_CHECK_T3                    8026
-#define IDC_CHECK_P1                    8027
-#define IDC_CHECK_P2                    8028
-#define IDC_CHECK_C1                    8029
-
-// Drop-down boxes
-#define IDC_PORT_VANNES                 8040
+#define IDC_EDIT_T4                     8016
+#define IDC_EDIT_T5                     8017
+#define IDC_EDIT_T6                     8018
+#define IDC_EDIT_P3                     8019
+#define IDC_EDIT_P4                     8020
+#define IDC_EDIT_C2                     8021
+#define IDC_EDIT_T7                     8022
+#define IDC_EDIT_T8                     8023
+#define IDC_EDIT_T9                     8024
+#define IDC_EDIT_P5                     8025
+#define IDC_EDIT_P6                     8026
+#define IDC_EDIT_C3                     8027
 #define IDC_COMBO_INSTRUMENT_T1         8041
 #define IDC_COMBO_INSTRUMENT_T2         8042
 #define IDC_COMBO_INSTRUMENT_T3         8043
@@ -28,6 +33,7 @@
 #define IDC_COMBO_INSTRUMENT_P2         8045
 #define IDC_COMBO_INSTRUMENT_C1         8046
 #define IDD_CONNECTION_PORT             8047
+#define IDC_COMBO_INSTRUMENT_T4         8047
 #define IDC_PORT_INSTRUMENT_1           8048
 #define IDC_PORT_INSTRUMENT_2           8049
 #define IDC_PORT_INSTRUMENT_3           8050
@@ -46,10 +52,42 @@
 #define IDC_COMBO_TYPE_INSTRUMENT_4     8063
 #define IDC_COMBO_TYPE_INSTRUMENT_5     8064
 #define IDC_COMBO_TYPE_INSTRUMENT_6     8065
+#define IDC_COMBO_CHANNEL_T4            8066
+#define IDC_COMBO_CHANNEL_T5            8067
+#define IDC_COMBO_CHANNEL_T6            8068
+#define IDC_COMBO_INSTRUMENT_T5         8069
+#define IDC_COMBO_CHANNEL_T7            8070
+#define IDC_COMBO_CHANNEL_T8            8071
+#define IDC_COMBO_INSTRUMENT_T6         8072
+#define IDC_COMBO_CHANNEL_T9            8073
+#define IDC_COMBO_CHANNEL_T10           8074
+#define IDC_COMBO_INSTRUMENT_T7         8075
+#define IDC_COMBO_CHANNEL_T11           8076
+#define IDC_COMBO_CHANNEL_T12           8077
+#define IDC_COMBO_INSTRUMENT_T8         8078
+#define IDC_COMBO_CHANNEL_T13           8079
+#define IDC_COMBO_CHANNEL_T14           8080
+#define IDC_COMBO_INSTRUMENT_T9         8081
+#define IDC_COMBO_CHANNEL_T15           8082
+#define IDC_COMBO_CHANNEL_T16           8083
+#define IDC_COMBO_INSTRUMENT_T10        8084
+#define IDC_COMBO_CHANNEL_T17           8085
+#define IDC_COMBO_CHANNEL_T18           8086
+#define IDC_COMBO_INSTRUMENT_T11        8087
+#define IDC_COMBO_CHANNEL_T19           8088
+#define IDC_COMBO_CHANNEL_T20           8089
+#define IDC_COMBO_INSTRUMENT_T12        8090
+#define IDC_COMBO_CHANNEL_T21           8091
+#define IDC_COMBO_CHANNEL_T22           8092
+#define IDC_COMBO_INSTRUMENT_T13        8093
+#define IDC_COMBO_CHANNEL_T23           8094
+#define IDC_COMBO_CHANNEL_T24           8095
+#define IDC_COMBO_INSTRUMENT_T14        8096
+#define IDC_COMBO_CHANNEL_T25           8097
 
-// Next default values for new objects	   
-// 										   
-#ifdef APSTUDIO_INVOKED					   
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
