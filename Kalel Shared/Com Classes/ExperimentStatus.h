5e7bcbcc9dbbafed23add5a5395a860559a94b89
Paul Iacomi <iacomi.paul@gmail.com>
2018-03-26 22:46:41 +0200

    attempt to write sync class

--- Kalel Shared/Com Classes/ExperimentStatus.h
+++ Kalel Shared/Com Classes/ExperimentStatus.h
@@ -3,6 +3,21 @@
 #include <atomic>
 #include <chrono>	// for timepoint
 
+
+template<typename T>
+struct atomic_ts : public std::atomic<T>
+{
+private:
+	std::chrono::system_clock::time_point& tp;
+public:
+	atomic_ts(std::chrono::system_clock::time_point& t) : tp(t) {};
+	atomic_ts& operator=(const T& a) {
+		std::atomic<T>::operator=(a);
+		return *this;
+	};
+	// using std::atomic<T>::operator=;
+};
+
 class ExperimentStatus
 {
 
@@ -16,45 +31,48 @@ public:
 	~ExperimentStatus();
 
 	void ResetData();
-	
+
 
 	// Overload equals function
 	ExperimentStatus & ExperimentStatus::operator=(const ExperimentStatus & p);
 
+
+	std::chrono::system_clock::time_point tp;
+	
 	///*******************
 	///		Global flags
 	///*******************
 
-	std::atomic_bool experimentInProgress;							// Boolean stating whether the experiment is started or not
-	std::atomic_bool experimentRecording;							// Boolean stating whether the data is being recorded at each measurement
-	std::atomic_bool experimentWaiting;								// Boolean stating the experiment is currently waiting
-	std::atomic_bool experimentCommandsRequested;					// Boolean stating that any automation is requested (manual,automatic etc)
+	atomic_ts<bool> experimentInProgress{ tp };							// Boolean stating whether the experiment is started or not
+	atomic_ts<bool> experimentRecording{ tp };							// Boolean stating whether the data is being recorded at each measurement
+	atomic_ts<bool> experimentWaiting{ tp };							// Boolean stating the experiment is currently waiting
+	atomic_ts<bool> experimentCommandsRequested{ tp };					// Boolean stating that any automation is requested (manual,automatic etc)
 
 	
 	///*******************
 	///		Parameters for storing where program has reached
 	///*******************
 
-	std::atomic_int experimentStage;								// Current experiment stage, the main part of the program (verification, equilibration, adsorption, desorption etc.)
-	std::atomic_int verificationStep;								// The security check steps
-	std::atomic_int experimentStepStatus;							// Current step status, for each step this can be STARTING, ENDING or IN_PROGRESS
-	std::atomic_int experimentSubstepStage;							// Current dose substep, can be at injection, equilibration etc
-	std::atomic_int experimentDose;									// Current experiment dose that is currently underway (ex: 2nd dose of adsorption step 1)
-	std::atomic_int experimentPreviousStage;						// Previous experiment stage
+	atomic_ts<int> experimentStage{ tp };								// Current experiment stage, the main part of the program (verification, equilibration, adsorption, desorption etc.)
+	atomic_ts<int> verificationStep{ tp };								// The security check steps
+	atomic_ts<int> experimentStepStatus{ tp };							// Current step status, for each step this can be STARTING, ENDING or IN_PROGRESS
+	atomic_ts<int> experimentSubstepStage{ tp };						// Current dose substep, can be at injection, equilibration etc
+	atomic_ts<int> experimentDose{ tp };								// Current experiment dose that is currently underway (ex: 2nd dose of adsorption step 1)
+	atomic_ts<int> experimentPreviousStage{ tp };						// Previous experiment stage
 
 	// Time
-	std::atomic_ullong timeStart;									// When the experiment started
-	std::atomic_ullong timeEquilibrationStart;						// Time equilibration started
-	std::atomic<double> timeToEquilibrate;							// The amount of time that the waiting + recording functionality will run before returning to an active state
+	atomic_ts<unsigned long long> timeStart{ tp };						// When the experiment started
+	atomic_ts<unsigned long long> timeEquilibrationStart{ tp };			// Time equilibration started
+	atomic_ts<double> timeToEquilibrate{ tp };							// The amount of time that the waiting + recording functionality will run before returning to an active state
 	
 	// Counters
-	std::atomic_int injectionAttemptCounter;						// Counter for the injections
-	std::atomic_int adsorptionCounter;								// Counter for the number of adsorption settings (small, medium, large etc) inputted by the user, starts at 0
-	std::atomic_int desorptionCounter;								// Counter for the number of desorption settings (small, medium, large etc) inputted by the user, starts at 0
+	atomic_ts<int> injectionAttemptCounter{ tp };						// Counter for the injections
+	atomic_ts<int> adsorptionCounter{ tp };								// Counter for the number of adsorption settings (small, medium, large etc) inputted by the user, starts at 0
+	atomic_ts<int> desorptionCounter{ tp };								// Counter for the number of desorption settings (small, medium, large etc) inputted by the user, starts at 0
 	
 	// Pressures used as benchmarks
-	std::atomic<double> pressureInitial;							// Pressure set as an initial pressure in injections
-	std::atomic<double> pressureFinal;								// Pressure set as the final pressure in injections
-	std::atomic<double> pressureHighOld;							// Previous pressure stored for injection checks
+	atomic_ts<double> pressureInitial{ tp };							// Pressure set as an initial pressure in injections
+	atomic_ts<double> pressureFinal{ tp };								// Pressure set as the final pressure in injections
+	atomic_ts<double> pressureHighOld{ tp };							// Previous pressure stored for injection checks
 
 };
